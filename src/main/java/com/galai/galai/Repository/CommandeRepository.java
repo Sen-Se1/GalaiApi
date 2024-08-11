0f7129d6f70400cb27e349afdbf6e4a5c99e6140
Mbarki Houssem <mbarkihoussem99@gmail.com>
2024-08-11 13:50:32 +0100

    fix bugs

--- src/main/java/com/galai/galai/Repository/CommandeRepository.java
+++ src/main/java/com/galai/galai/Repository/CommandeRepository.java
@@ -3,7 +3,6 @@ package com.galai.galai.Repository;
 import com.galai.galai.Entity.Commande;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 public interface CommandeRepository extends JpaRepository<Commande, Integer> {
 }
--- src/main/java/com/galai/galai/Repository/UserRepository.java
+++ src/main/java/com/galai/galai/Repository/UserRepository.java
@@ -2,8 +2,6 @@ package com.galai.galai.Repository;
 
 import com.galai.galai.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import javax.swing.text.html.Option;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
